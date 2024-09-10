911f7557d68d1e1e1421c0ffa1ccac506f5089b0
vivekvarma2003 <vv674717@gmail.com>
2024-09-10 18:03:38 +0530

    added new function process_file

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import os
 import glob
 import pandas as pd
 
+# problems in the code
+# Folder Paths are hard coded     -> use environment variables to avoid it 
+# modularization and reusibility  
+# modularization -> dividing large piece of code into small pieces
+
 
 def get_columns(ds):
     BASE_DIR = os.environ.setdefault('BASE_DIR','data/retail_db/schemas.json')
@@ -20,6 +25,16 @@ def get_columns(ds):
         print(f'Schema not found for {ds}')
         return
 
+def process_file(SRC_BASE_DIR,TGT_BASE_DIR,ds):
+    for file in glob.glob(f'{SRC_BASE_DIR}/{ds}/*'):
+        df = pd.read_csv(file, names = get_columns(ds))
+        os.makedirs(f'{TGT_BASE_DIR}/{ds}', exist_ok=True)
+        df.to_json(
+            f'{TGT_BASE_DIR}/{ds}/part-{str(uuid.uuid1())}.json',
+            orient = 'records',
+            lines = True 
+        )
+        print(f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
 
 def convert_csv_to_json():
     SRC_BASE_DIR = os.environ.setdefault('SRC_BASE_DIR','data/retail_db')
@@ -28,14 +43,6 @@ def convert_csv_to_json():
     for path in glob.glob(f'{SRC_BASE_DIR}/*'):
         if os.path.isdir(path):
             ds = os.path.split(path)[1]
-            for file in glob.glob(f'{path}/*'):
-                df = pd.read_csv(file, names = get_columns(ds))
-                os.makedirs(f'{TGT_BASE_DIR}/{ds}', exist_ok=True)
-                df.to_json(
-                    f'{TGT_BASE_DIR}/{ds}/part-{str(uuid.uuid1())}.json',
-                    orient = 'records',
-                    lines = True 
-                )
-                print(f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+            process_file(SRC_BASE_DIR, TGT_BASE_DIR, ds)
 if __name__=='__main__':
     convert_csv_to_json()
